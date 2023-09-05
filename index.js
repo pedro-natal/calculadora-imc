f83cd0be131cb467c3929da3f923e026f009bb8c
pedro-natal <pedropferreiranatal@gmail.com>
2023-09-05 16:29:34 -0300

    refatoração da pasta "./imagens"

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ function calculoIMC() {
   var imc = peso / altura ** 2;
   validação(peso, altura);
   if (imc < 18.5) {
-    resultado(imc, "#26abe3", "abaixo do peso", "./imagens/img1.png");
+    resultado(imc, "#26abe3", "abaixo do peso", "./images/img1.png");
   } else if (imc >= 18.5 && imc <= 24.9) {
-    resultado(imc, "#6b7f36", "no peso normal", "./imagens/img2.png");
+    resultado(imc, "#6b7f36", "no peso normal", "./images/img2.png");
   } else if (imc >= 25 && imc <= 29.9) {
-    resultado(imc, "#f0a91d", "com excesso de peso", "./imagens/img3.png");
+    resultado(imc, "#f0a91d", "com excesso de peso", "./images/img3.png");
   } else if (imc >= 30 && imc <= 39.9) {
-    resultado(imc, "#e9752c", "com obesidade grau 1", "./imagens/img4.png");
+    resultado(imc, "#e9752c", "com obesidade grau 1", "./images/img4.png");
   } else {
-    resultado(imc, "#d92026", "com obesidade grau 2", "./imagens/img5.png");
+    resultado(imc, "#d92026", "com obesidade grau 2", "./images/img5.png");
   }
 }
 
